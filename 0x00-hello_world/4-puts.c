7e6f35c3351581a27faccbbbd4d0dbc93a6f2329
Charawey-X <charawey@gmail.com>
2022-03-10 22:12:55 +0300

    Using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void)
+{
+	/*initialize string*/
+	char Mystr[]="Programming is like building a multilingual puzzle";
+	/*writing string to stdout*/
+	puts(Mystr);
+
+	return 0;
